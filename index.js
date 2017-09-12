fc2528880b8e1ca878699e19628b4497bc9f4ddc
Kyle Liu <kyle116@yahoo.com>
2017-09-12 14:40:27 -0700

    time added

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const
 
 let nightmare = Nightmare()
 
-nightmare
+var currentdate = new Date();
+var datetime = "Last Ran: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+
+// var repeat = function() {
+  nightmare
   .goto('https://profiles.generalassemb.ly/profiles')
+  .wait('body > nav > div.nav__login-ctas.js-signed-out > a.nav__login-ctas__sign-in.emphasis.js-sign-in-nav-bar')
   .click('body > nav > div.nav__login-ctas.js-signed-out > a.nav__login-ctas__sign-in.emphasis.js-sign-in-nav-bar')
   .wait('#user_email')
   .type('#user_email', process.env.username)
@@ -32,13 +42,22 @@ nightmare
   .click('#edit_profile_8812 > div > input')
   .wait('body > div.form-page-container.new-profile > div.main-container > div > header > p')
   .click('body > div.form-page-container.new-profile > div.sidebar > div > div > nav > a')
-  .run(function(err, nightmare) {
+  .then(function(err, nightmare) {
     if (err) {
       console.log(err);
     } else {
-      console.log('Done');
+      console.log('Done' + datetime);
     }
   })
+// }
+
+// setInterval(repeat, 10000)
+
+// var hello = function() {
+//   console.log('hfsdf');
+// }
+//
+// setInterval(hello, 3000)
 
 app.get('/', (req, res) => {
 
